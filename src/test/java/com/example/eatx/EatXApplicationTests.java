198fcac74c1557e318deedf5fde82b13eb310232
Jakub Bartus <256466@student.pwr.edu.pl>
2021-12-13 12:55:10 +0100

    FrontEnd 1

---
+++ src/test/java/com/example/eatx/EatXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eatx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
